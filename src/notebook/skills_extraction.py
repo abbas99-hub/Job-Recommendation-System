3260eae60b602cae60ef12000213cebb58af4a66
Abbas Behrainwala <abbasbehrain95@gmail.com>
2023-05-15 20:14:41 +0530

    Completed Job Recommendation System in Jupyter Notebook

---
+++ src/notebook/skills_extraction.py
@@ -0,0 +1,56 @@
+import spacy
+from spacy.matcher import Matcher
+import PyPDF2
+
+# Load the Spacy English model
+nlp = spacy.load('en_core_web_sm')
+import csv
+from spacy.matcher import Matcher
+import csv
+
+# Read skills from CSV file
+with open('skills.csv', 'r') as file:
+    csv_reader = csv.reader(file)
+    skills = [row for row in csv_reader]
+
+# Create pattern dictionaries from skills
+skill_patterns = [[{'LOWER': skill}] for skill in skills[0]]
+
+# Create a Matcher object
+matcher = Matcher(nlp.vocab)
+
+# Add skill patterns to the matcher
+for pattern in skill_patterns:
+    matcher.add('Skills', [pattern])
+
+# Function to extract skills from text
+def extract_skills(text):
+    doc = nlp(text)
+    matches = matcher(doc)
+    skills = set()
+    for match_id, start, end in matches:
+        skill = doc[start:end].text
+        skills.add(skill)
+    return skills
+
+# Function to extract text from PDF
+def extract_text_from_pdf(file_path:str):
+    with open(file_path, 'rb') as f:
+        pdf_reader = PyPDF2.PdfReader(f)
+        text = ''
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def skills_extractor(file_path):
+        # Path to the resume PDF file
+        resume_file = file_path
+
+        # Extract text from PDF
+        resume_text = extract_text_from_pdf(resume_file)
+
+        # Extract skills from resume text
+        skills = list(extract_skills(resume_text))
+        return skills
+
+
